26bb086da0434990ed5089de9ab601c9978fd726
farialammoumi <99811859+farialammoumi@users.noreply.github.com>
2024-12-15 20:53:03 +0600

    Add files via upload

---
+++ Supermarket1/src/main/java/com/mycompany/supermarket1/Supermarket1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.supermarket1;
+
+/**
+ *
+ * @author HR COMPUTER FAIR
+ */
+public class Supermarket1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
